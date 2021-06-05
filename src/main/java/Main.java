37abbe4ce411e1261a4d635880b1917baf027167
alexis-allemann <alexis.allemann@gmail.com>
2021-06-05 08:24:31 +0200

    Java 11 with Maven project created

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+/**
+ * Main application class
+ *
+ * @author Allemann, Balestrieri, Christen, Mottier, Zeller
+ * @version 1.0
+ */
+public class Main {
+
+    /**
+     * Main application method
+     *
+     * @param args no args required
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+    }
+}
